99d209908c186fccc08d7059ddd1495f5680c949
erbo <erbo_shan@sfu.ca>
2019-09-26 20:58:04 -0700

    Q2

---
+++ 2.add-two-numbers.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=2 lang=cpp
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (31.86%)
+ * Likes:    6028
+ * Dislikes: 1576
+ * Total Accepted:    1M
+ * Total Submissions: 3.2M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        int sum = 0;
+        ListNode *ret = nullptr;
+        ListNode **node = &ret;
+        while (l1 != nullptr || l2 != nullptr || sum != 0)
+        {
+            if (l1 != nullptr)
+            {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr)
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            (*node) = new ListNode(sum % 10);
+            sum /= 10;
+            node = &((*node)->next);
+        }
+        return ret;
+    }
+};
