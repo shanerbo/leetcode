d716a3dae3815ed8c468660748993d292595a33e
erbo <erbo_shan@sfu.ca>
2019-09-26 20:21:09 -0700

    Q1

---
+++ 1.two-sum.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        std::map<int, int> dict;
+        std::vector<int> ret;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            dict.insert({target - nums[i], i});
+            auto it = dict.find(nums[i]);
+            if (it != dict.end() && i != it->second)
+            {
+                ret = {it->second, i};
+            }
+        }
+        return ret;
+    }
+}
+
+;
