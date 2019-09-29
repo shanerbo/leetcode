2f99ec8c836d076ea0b69dfd288fefccba8e9169
erbo <erbo_shan@sfu.ca>
2019-09-28 22:26:33 -0700

    Q11

---
+++ 11.container-with-most-water.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=11 lang=cpp
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (46.83%)
+ * Likes:    3983
+ * Dislikes: 476
+ * Total Accepted:    454.1K
+ * Total Submissions: 969K
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a
+ * point at coordinate (i, ai). n vertical lines are drawn such that the two
+ * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+ * with x-axis forms a container, such that the container contains the most
+ * water.
+ * 
+ * Note: You may not slant the container and n is at least 2.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In
+ * this case, the max area of water (blue section) the container can contain is
+ * 49. 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * 
+ */
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int maxArea = 0;
+        auto l = height.begin(), r = height.end() - 1;
+
+        while (std::distance(l, r) > 0)
+        {
+            int height = std::min(*l, *r);
+            int d = std::distance(l, r);
+            maxArea = height * d;
+            while (height > *l && std::distance(l, r) > 0)
+            {
+                l++;
+            }
+            while (height > *r && std::distance(l, r) > 0)
+            {
+                r--;
+            }
+        }
+
+        return maxArea;
+    }
+};
