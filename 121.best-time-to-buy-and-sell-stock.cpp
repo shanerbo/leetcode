ff1956f7d3e0760f70516370715af575f2386484
shanerbo <shanerbo@gmail.com>
2019-10-07 22:37:52 -0700

    Q121

---
+++ 121.best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=121 lang=cpp
+ *
+ * [121] Best Time to Buy and Sell Stock
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ *
+ * algorithms
+ * Easy (48.34%)
+ * Likes:    3215
+ * Dislikes: 146
+ * Total Accepted:    604.4K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * Say you have an array for which the i^th element is the price of a given
+ * stock on day i.
+ * 
+ * If you were only permitted to complete at most one transaction (i.e., buy
+ * one and sell one share of the stock), design an algorithm to find the
+ * maximum profit.
+ * 
+ * Note that you cannot sell a stock before you buy one.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit
+ * = 6-1 = 5.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        if (prices.size() < 1)
+            return 0;
+        int min = INT_MAX, max = INT_MIN;
+        for (auto &price : prices) {
+            min = std::min(price, min);
+            max = std::max(price - min, max);
+        }
+        return max;
+    }
+};
+// @lc code=end
