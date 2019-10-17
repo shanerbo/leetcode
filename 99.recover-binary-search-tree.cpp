20f362f3f6e9c3e7f810eaebcdc1b48506576513
shanerbo <shanerbo@gmail.com>
2019-10-16 17:27:55 -0700

    Q99 recursive

---
+++ 99.recover-binary-search-tree.cpp
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode id=99 lang=cpp
+ *
+ * [99] Recover Binary Search Tree
+ *
+ * https://leetcode.com/problems/recover-binary-search-tree/description/
+ *
+ * algorithms
+ * Hard (35.97%)
+ * Likes:    971
+ * Dislikes: 55
+ * Total Accepted:    131K
+ * Total Submissions: 364K
+ * Testcase Example:  '[1,3,null,null,2]'
+ *
+ * Two elements of a binary search tree (BST) are swapped by mistake.
+ * 
+ * Recover the tree without changing its structure.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,3,null,null,2]
+ * 
+ * 1
+ * /
+ * 3
+ * \
+ * 2
+ * 
+ * Output: [3,1,null,null,2]
+ * 
+ * 3
+ * /
+ * 1
+ * \
+ * 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,1,4,null,null,2]
+ * 
+ * ⁠ 3
+ * ⁠/ \
+ * 1   4
+ * /
+ * 2
+ * 
+ * Output: [2,1,4,null,null,3]
+ * 
+ * ⁠ 2
+ * ⁠/ \
+ * 1   4
+ * /
+ * ⁠ 3
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * A solution using O(n) space is pretty straight forward.
+ * Could you devise a constant space solution?
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  private:
+    void dfs(TreeNode *root, TreeNode *&pre, TreeNode *&first, TreeNode *&second) {
+        if (!root) {
+            return;
+        } else {
+            dfs(root->left, pre, first, second);
+            if (pre && pre->val > root->val) {
+                if (!first)
+                    first = pre;
+                second = root;
+            }
+            pre = root;
+            dfs(root->right, pre, first, second);
+        }
+    }
+
+  public:
+    void recoverTree(TreeNode *root) {
+        if (!root) {
+            return;
+        } else {
+            // inorder traversal
+            TreeNode *pre = nullptr, *first = nullptr, *second = nullptr;
+            dfs(root, pre, first, second);
+            if (first)
+                swap(first->val, second->val);
+        }
+    }
+};
+// @lc code=end
