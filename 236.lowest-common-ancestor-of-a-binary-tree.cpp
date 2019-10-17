74b15a59d00f267c6c5eb0b725887e8958c3ab69
erbo <erbo_shan@sfu.ca>
2019-10-17 15:15:36 -0700

    Q236

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=236 lang=cpp
+ *
+ * [236] Lowest Common Ancestor of a Binary Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+ *
+ * algorithms
+ * Medium (40.05%)
+ * Likes:    2447
+ * Dislikes: 145
+ * Total Accepted:    336.4K
+ * Total Submissions: 836.6K
+ * Testcase Example:  '[3,5,1,6,2,0,8,null,null,7,4]\n5\n1'
+ *
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given
+ * nodes in the tree.
+ * 
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ * 
+ * Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * Output: 3
+ * Explanation: The LCA of nodes 5 and 1 is 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * Output: 5
+ * Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant
+ * of itself according to the LCA definition.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the binary tree.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  private:
+    TreeNode *dfs(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (!root || root == p || root == q) {
+            return root;
+        } else {
+            TreeNode *l = dfs(root->left, p, q);
+            TreeNode *r = dfs(root->right, p, q);
+            if (l && r) { // p and q found
+                return root;
+            } else if (!l) {
+                return r;
+            } else {
+                return l;
+            }
+        }
+    }
+
+  public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (!root) {
+            return nullptr;
+        } else {
+            return dfs(root, p, q);
+        }
+    }
+};
+// @lc code=end
