68f55b1bed60d33de0d681c0b58825dacb8c303f
erbo <erbo_shan@sfu.ca>
2019-10-17 16:56:15 -0700

    Q41 EA

---
+++ 41.first-missing-positive.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=41 lang=cpp
+ *
+ * [41] First Missing Positive
+ *
+ * https://leetcode.com/problems/first-missing-positive/description/
+ *
+ * algorithms
+ * Hard (29.92%)
+ * Likes:    2186
+ * Dislikes: 655
+ * Total Accepted:    251.2K
+ * Total Submissions: 838.3K
+ * Testcase Example:  '[1,2,0]'
+ *
+ * Given an unsorted integer array, find the smallest missing positive
+ * integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,0]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,4,-1,1]
+ * Output: 2
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [7,8,9,11,12]
+ * Output: 1
+ * 
+ * 
+ * Note:
+ * 
+ * Your algorithm should run in O(n) time and uses constant extra space.
+ * 
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    int firstMissingPositive(vector<int> &nums) {
+    }
+};
+// @lc code=end
