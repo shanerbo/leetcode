d34dd789dc92098582f8edcfb25781ea0f4994a3
shanerbo <shanerbo@gmail.com>
2019-10-19 16:07:56 -0700

    Q86 Q561 Q674

---
+++ 86.partition-list.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=86 lang=cpp
+ *
+ * [86] Partition List
+ *
+ * https://leetcode.com/problems/partition-list/description/
+ *
+ * algorithms
+ * Medium (38.69%)
+ * Likes:    837
+ * Dislikes: 223
+ * Total Accepted:    181.9K
+ * Total Submissions: 469.8K
+ * Testcase Example:  '[1,4,3,2,5,2]\n3'
+ *
+ * Given a linked list and a value x, partition it such that all nodes less
+ * than x come before nodes greater than or equal to x.
+ * 
+ * You should preserve the original relative order of the nodes in each of the
+ * two partitions.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: head = 1->4->3->2->5->2, x = 3
+ * Output: 1->2->2->4->3->5
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        
+    }
+};
+// @lc code=end
+
