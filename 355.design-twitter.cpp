2da103e5b164458be454fba03183615b5c904597
erbo <erbo_shan@sfu.ca>
2019-10-25 00:44:20 -0700

    Q355 not my work, this question is totally waste of time

--- 355.design-twitter.cpp
+++ 355.design-twitter.cpp
@@ -62,118 +62,64 @@
 
 // @lc code=start
 class Twitter {
-  private:
-    struct User {
-        int userId;
-        vector<pair<int, int>> tweets;
-        vector<User *> follower;
-        vector<User *> following;
-        User(int _userid, pair<int, int> _pair) {
-            userId = _userid;
-            tweets.push_back(_pair);
-        }
-        User(int _userid) {
-            userId = _userid;
-        }
+    struct Tweet {
+        int time;
+        int id;
+        Tweet(int time, int id) : time(time), id(id) {}
     };
-    unordered_map<int, User *> twitter; // <userId, user DataStructure>
+
+    std::unordered_map<int, std::vector<Tweet>> tweets;         // [u] = array of tweets by u
+    std::unordered_map<int, std::unordered_set<int>> following; // [u] = array of users followed by u
+
+    int time;
 
   public:
-    /** Initialize your data structure here. */
-    Twitter() {
-    }
+    Twitter() : time(0) {}
 
-    /** Compose a new tweet. */
     void postTweet(int userId, int tweetId) {
-        if (twitter.find(userId) == twitter.end()) {
-            User *user = new User(userId, make_pair(userId, tweetId));
-            twitter[userId] = user;
-        } else {
-            User *user = twitter[userId];
-            pair<int, int> tweet = make_pair(userId, tweetId);
-            user->tweets.push_back(tweet);
-            for (auto follower : user->follower) {
-                follower->tweets.push_back(tweet);
-            }
-        }
+        tweets[userId].emplace_back(time++, tweetId);
     }
 
-    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-    vector<int> getNewsFeed(int userId) {
-        vector<int> tweets;
-        if (twitter.find(userId) == twitter.end())
-            return {};
-        User *user = twitter[userId];
-        auto userTweets = user->tweets;
-        // auto cmp = [](pair<int, int> a, pair<int, int> b) { return a.second < b.second; };
-        // sort(userTweets.begin(), userTweets.end(), cmp);
-        int len = userTweets.size() >= 10 ? 10 : userTweets.size();
-        for (int i = userTweets.size() - 1; len > 0; --i, --len) {
-            tweets.push_back(userTweets[i].second);
+    std::vector<int> getNewsFeed(int userId) {
+        std::vector<std::pair<Tweet *, Tweet *>> h; // pair of pointers (begin, current)
+
+        for (auto &u : following[userId]) {
+            auto &t = tweets[u];
+            if (t.size() > 0)
+                h.emplace_back(t.data(), t.data() + t.size() - 1);
         }
-        return tweets;
+        auto &t = tweets[userId]; // self
+        if (t.size() > 0)
+            h.emplace_back(t.data(), t.data() + t.size() - 1);
+
+        auto f = [](const std::pair<Tweet *, Tweet *> &x, const std::pair<Tweet *, Tweet *> &y) {
+            return x.second->time < y.second->time;
+        };
+        std::make_heap(h.begin(), h.end(), f);
+
+        const int n = 10;
+        std::vector<int> o;
+        o.reserve(n);
+        for (int i = 0; (i < n) && !h.empty(); ++i) {
+            std::pop_heap(h.begin(), h.end(), f);
+
+            auto &hb = h.back();
+            o.push_back(hb.second->id);
+
+            if (hb.first == hb.second--)
+                h.pop_back();
+            else
+                std::push_heap(h.begin(), h.end(), f);
+        }
+        return o;
     }
 
-    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-    void
-    follow(int followerId, int followeeId) {
-        if (twitter.find(followeeId) == twitter.end()) {
-            User *user = new User(followeeId);
-            twitter[followeeId] = user;
-        }
-        if (twitter.find(followerId) == twitter.end()) {
-            User *user = new User(followerId);
-            twitter[followerId] = user;
-        }
-        twitter[followeeId]->follower.push_back(twitter[followerId]);
-        twitter[followerId]->following.push_back(twitter[followeeId]);
-        for (auto t : twitter[followeeId]->tweets) {
-            twitter[followerId]->tweets.push_back(t);
-        }
-        // auto cmp = [](pair<int, int> a, pair<int, int> b) { return a.second < b.second; };
-        // sort(twitter[followerId]->tweets.begin(), twitter[followerId]->tweets.end(), cmp);
+    void follow(int followerId, int followeeId) {
+        if (followerId != followeeId)
+            following[followerId].insert(followeeId);
     }
 
-    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     void unfollow(int followerId, int followeeId) {
-        auto *user = twitter[followerId];
-        auto following = user->following;
-        for (auto it = following.begin(); it != following.end();) {
-            if ((*it)->userId == followeeId) {
-                following.erase(it);
-            } else {
-                it++;
-            }
-        }
-
-        auto *tweets = &user->tweets;
-        for (auto it = tweets->begin(); it != tweets->end();) {
-            if ((*it).first == followeeId) {
-                tweets->erase(it);
-                // cout << (*it).second << endl;
-            } else {
-                it++;
-            }
-        }
-
-        auto *user2 = twitter[followeeId];
-        auto following2 = user->following;
-        for (auto it = following2.begin(); it != following2.end();) {
-            if ((*it)->userId == followerId) {
-                following2.erase(it);
-            } else {
-                it++;
-            }
-        }
+        following[followerId].erase(followeeId);
     }
 };
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter* obj = new Twitter();
- * obj->postTweet(userId,tweetId);
- * vector<int> param_2 = obj->getNewsFeed(userId);
- * obj->follow(followerId,followeeId);
- * obj->unfollow(followerId,followeeId);
- */
-// @lc code=end
