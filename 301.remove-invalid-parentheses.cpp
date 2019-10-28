51eaab2e9eea7ba4b164f21848115a49738e8374
erbo <erbo_shan@sfu.ca>
2019-10-28 13:12:50 -0700

    Q301 TODO: solve it with DFS

---
+++ 301.remove-invalid-parentheses.cpp
@@ -0,0 +1,128 @@
+/*
+ * @lc app=leetcode id=301 lang=cpp
+ *
+ * [301] Remove Invalid Parentheses
+ *
+ * https://leetcode.com/problems/remove-invalid-parentheses/description/
+ *
+ * algorithms
+ * Hard (40.56%)
+ * Likes:    1826
+ * Dislikes: 75
+ * Total Accepted:    154.4K
+ * Total Submissions: 379.1K
+ * Testcase Example:  '"()())()"'
+ *
+ * Remove the minimum number of invalid parentheses in order to make the input
+ * string valid. Return all possible results.
+ * 
+ * Note: The input string may contain letters other than the parentheses ( and
+ * ).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()())()"
+ * Output: ["()()()", "(())()"]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "(a)())()"
+ * Output: ["(a)()()", "(a())()"]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: ")("
+ * Output: [""]
+ * 
+ */
+
+// @lc code=start
+class Solution1 {
+
+  private:
+    bool check(string &s) {
+        int count = 0;
+        for (auto const &c : s) {
+            if (c == '(') {
+                count++;
+            }
+            if (c == ')' && count-- == 0) { // no '('
+                return false;
+            }
+        }
+        return count == 0;
+    };
+
+  public:
+    vector<string> removeInvalidParentheses(string s) {
+        vector<string> res;
+        set<string> visited;
+        // sanity check
+        if (s.length() < 0) {
+            return res;
+        }
+        queue<string> q;
+        q.push(s);
+        bool found = false;
+        while (q.size() > 0) {
+            s = q.front();
+            q.pop();
+            if (check(s)) {
+                res.push_back(s);
+                found = true;
+                // this will make sure we generate smallest results
+            }
+            if (found) {
+                continue;
+            }
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] != '(' && s[i] != ')') {
+                    continue;
+                }
+                string t = s.substr(0, i) + s.substr(i + 1);
+                // cout << t << endl;
+                if (visited.insert(t).second) {
+                    // if t already tested we have to skip it
+                    // use set to track it
+                    q.push(t);
+                }
+            }
+        }
+
+        return res;
+    }
+};
+class Solution {
+
+  private:
+    bool check(string &s) {
+        int count = 0;
+        for (auto const &c : s) {
+            if (c == '(') {
+                count++;
+            }
+            if (c == ')' && count-- == 0) { // no '('
+                return false;
+            }
+        }
+        return count == 0;
+    };
+
+  public:
+    vector<string> removeInvalidParentheses(string s) {
+        vector<string> res;
+        set<string> visited;
+        // sanity check
+        if (s.length() < 0) {
+            return res;
+        }
+
+        return res;
+    }
+};
+// @lc code=end
