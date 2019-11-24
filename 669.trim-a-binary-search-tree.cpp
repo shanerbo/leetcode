ce335104af685835098a86a412dbbf9180935752
shanerbo <shanerbo@gmail.com>
2019-11-23 22:56:56 -0800

    Q669

---
+++ 669.trim-a-binary-search-tree.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=669 lang=cpp
+ *
+ * [669] Trim a Binary Search Tree
+ *
+ * https://leetcode.com/problems/trim-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (61.16%)
+ * Likes:    1348
+ * Dislikes: 142
+ * Total Accepted:    80K
+ * Total Submissions: 130.6K
+ * Testcase Example:  '[1,0,2]\n1\n2'
+ *
+ * 
+ * Given a binary search tree and the lowest and highest boundaries as L and R,
+ * trim the tree so that all its elements lies in [L, R] (R >= L). You might
+ * need to change the root of the tree, so the result should return the new
+ * root of the trimmed binary search tree.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 0   2
+ * 
+ * ⁠ L = 1
+ * ⁠ R = 2
+ * 
+ * Output: 
+ * ⁠   1
+ * ⁠     \
+ * ⁠      2
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 0   4
+ * ⁠  \
+ * ⁠   2
+ * ⁠  /
+ * ⁠ 1
+ * 
+ * ⁠ L = 1
+ * ⁠ R = 3
+ * 
+ * Output: 
+ * ⁠     3
+ * ⁠    / 
+ * ⁠  2   
+ * ⁠ /
+ * ⁠1
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    TreeNode *trimBST(TreeNode *root, int L, int R) {
+        if (!root) {
+            return nullptr;
+        }
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
+        return root->val < L ? root->right : root->val > R ? root->left : root;
+    }
+};
+// @lc code=end
