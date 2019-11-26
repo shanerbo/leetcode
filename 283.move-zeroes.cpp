29dabacfba4ea96505bd37f63e537e084b5f5703
shanerbo <shanerbo@gmail.com>
2019-11-25 21:09:01 -0800

    Q283

--- 283.move-zeroes.cpp
+++ 283.move-zeroes.cpp
@@ -48,4 +48,3 @@ class Solution {
         }
     }
 };
-// @lc code=end
---
+++ 314. Binary-Tree-Vertical-Order-Traversal.cpp
@@ -0,0 +1,10 @@
+/*
+ * @lc app=leetcode id=314 lang=cpp
+ */
+// @lc code=start
+class Solution {
+  public:
+    vector<vector<int>> verticalOrder(TreeNode *root){
+
+    };
+    // @lc code=end
