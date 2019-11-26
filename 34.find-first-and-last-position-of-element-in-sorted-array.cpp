163a37dc2ec71217881bdfd552226677b94f4b95
shanerbo <shanerbo@gmail.com>
2019-11-26 00:17:44 -0800

    Q34

--- 34.find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -41,10 +41,10 @@ class Solution {
         int l = 0, r = nums.size() - 1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
+            if (nums[mid] >= target) {
                 r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
         return l;
