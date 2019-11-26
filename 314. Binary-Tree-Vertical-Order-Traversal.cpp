263e47c694ea4594b4e9709f03f498bcdc327096
shanerbo <shanerbo@gmail.com>
2019-11-25 21:24:23 -0800

    Q314

--- 314. Binary-Tree-Vertical-Order-Traversal.cpp
+++ 314. Binary-Tree-Vertical-Order-Traversal.cpp
@@ -4,7 +4,33 @@
 // @lc code=start
 class Solution {
   public:
-    vector<vector<int>> verticalOrder(TreeNode *root){
-
-    };
-    // @lc code=end
+    vector<vector<int>> verticalOrder(TreeNode *root) {
+        vector<vector<int>> res;
+        if (!root) {
+            return res;
+        }
+        map<int, vector<int>> map;
+        queue<pair<int, TreeNode *>> queue;
+        queue.push(make_pair(5000, root));
+        while (!queue.empty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                auto tmp = queue.front();
+                queue.pop();
+                int col = tmp.first;
+                map[col].push_back(tmp.second->val);
+                if (tmp.second->left) {
+                    queue.push(make_pair(col - 1, tmp.second->left));
+                }
+                if (tmp.second->right) {
+                    queue.push(make_pair(col + 1, tmp.second->right));
+                }
+            }
+        }
+        for (auto const &m : map) {
+            res.push_back(m.second);
+        }
+        return res;
+    }
+};
+// @lc code=end
