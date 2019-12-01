1bd7d03f5ad0be4372652bc77c06fe7dd81756b3
shanerbo <shanerbo@gmail.com>
2019-11-30 22:19:45 -0800

    Q935

---
+++ 935.knight-dialer.cpp
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=935 lang=cpp
+ *
+ * [935] Knight Dialer
+ *
+ * https://leetcode.com/problems/knight-dialer/description/
+ *
+ * algorithms
+ * Medium (42.50%)
+ * Likes:    348
+ * Dislikes: 127
+ * Total Accepted:    21.6K
+ * Total Submissions: 50.5K
+ * Testcase Example:  '1'
+ *
+ * A chess knight can move as indicated in the chess diagram below:
+ * 
+ * .           
+ * 
+ * 
+ * 
+ * This time, we place our chess knight on any numbered key of a phone pad
+ * (indicated above), and the knight makes N-1 hops.  Each hop must be from one
+ * key to another numbered key.
+ * 
+ * Each time it lands on a key (including the initial placement of the knight),
+ * it presses the number of that key, pressing N digits total.
+ * 
+ * How many distinct numbers can you dial in this manner?
+ * 
+ * Since the answer may be large, output the answer modulo 10^9 + 7.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1
+ * Output: 10
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2
+ * Output: 20
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 3
+ * Output: 46
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= N <= 5000
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    int knightDialer(int n) {
+        int mod = 1000000000 + 7;
+        int res = 0;
+        vector<vector<int>> pad = {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
+        vector<vector<int>> dp(n, vector<int>(10, 0));
+        for (int i = 0; i < 10; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < 10; j++) {
+                auto fromWhere = pad[j];
+                for (auto const &f : fromWhere) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][f]) % mod;
+                }
+            }
+        }
+        for (int i = 0; i < 10; ++i) {
+            res = (res + dp[n - 1][i]) % mod;
+        }
+        return res;
+    }
+};
+// @lc code=end
