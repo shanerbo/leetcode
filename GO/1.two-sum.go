b961d2f1d34fdd8beea708ce2cc2be01fc0bba87
shanerbo <shanerbo@gmail.com>
2020-08-04 15:10:31 -0700

    add question 1

--- GO/1.two-sum.go
+++ GO/1.two-sum.go
@@ -15,24 +15,34 @@
  *
  * Given an array of integers, return indices of the two numbers such that they
  * add up to a specific target.
- * 
+ *
  * You may assume that each input would have exactly one solution, and you may
  * not use the same element twice.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Given nums = [2, 7, 11, 15], target = 9,
- * 
+ *
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 func twoSum(nums []int, target int) []int {
-    
-}
-// @lc code=end
+	m := make(map[int]int)
+	for idx, val := range nums {
+		m[target-val] = idx
+	}
+	for idx, val := range nums {
+		if value, ok := m[val]; ok && value != idx {
+			return []int{idx, value}
+		}
 
+	}
+	return []int{}
+}
+
+// @lc code=end
