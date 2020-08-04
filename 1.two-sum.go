45412b3681699b1cffb78c4e27fbc08197588a9f
shanerbo <shanerbo@gmail.com>
2020-08-04 14:35:06 -0700

    updating

---
+++ 1.two-sum.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (44.57%)
+ * Likes:    12050
+ * Dislikes: 419
+ * Total Accepted:    2.2M
+ * Total Submissions: 4.9M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+    
+}
+// @lc code=end
+
---
+++ 28.implement-str-str.go
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=28 lang=golang
+ *
+ * [28] Implement strStr()
+ *
+ * https://leetcode.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (33.08%)
+ * Likes:    1091
+ * Dislikes: 1525
+ * Total Accepted:    512.7K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * Implement strStr().
+ * 
+ * Return the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * 
+ * 
+ * Clarification:
+ * 
+ * What should we return when needle is an empty string? This is a great
+ * question to ask during an interview.
+ * 
+ * For the purpose of this problem, we will return 0 when needle is an empty
+ * string. This is consistent to C's strstr() and Java's indexOf().
+ * 
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+    
+}
+// @lc code=end
+
