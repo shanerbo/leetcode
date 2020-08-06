01714645c3bb843689c26643c19af515f7f20363
shanerbo <shanerbo@gmail.com>
2020-08-06 15:24:12 -0700

    q117

--- C++/117.populating-next-right-pointers-in-each-node-ii.cpp
+++ C++/117.populating-next-right-pointers-in-each-node-ii.cpp
@@ -1,21 +1,19 @@
 /*
- * @lc app=leetcode id=116 lang=cpp
+ * @lc app=leetcode id=117 lang=cpp
  *
- * [116] Populating Next Right Pointers in Each Node
+ * [117] Populating Next Right Pointers in Each Node II
  *
- * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
  *
  * algorithms
- * Medium (44.91%)
- * Likes:    2105
- * Dislikes: 145
- * Total Accepted:    371.1K
- * Total Submissions: 822.3K
- * Testcase Example:  '[1,2,3,4,5,6,7]'
+ * Medium (38.91%)
+ * Likes:    1643
+ * Dislikes: 180
+ * Total Accepted:    259K
+ * Total Submissions: 662.7K
+ * Testcase Example:  '[1,2,3,4,5,null,7]'
  *
- * You are given a perfect binary tree where all leaves are on the same level,
- * and every parent has two children. The binary tree has the following
- * definition:
+ * Given a binary tree
  *
  *
  * struct Node {
@@ -47,20 +45,20 @@
  *
  *
  *
- * Input: root = [1,2,3,4,5,6,7]
- * Output: [1,#,2,3,#,4,5,6,7,#]
- * Explanation: Given the above perfect binary tree (Figure A), your function
- * should populate each next pointer to point to its next right node, just like
- * in Figure B. The serialized output is in level order as connected by the
- * next pointers, with '#' signifying the end of each level.
+ * Input: root = [1,2,3,4,5,null,7]
+ * Output: [1,#,2,3,#,4,5,7,#]
+ * Explanation: Given the above binary tree (Figure A), your function should
+ * populate each next pointer to point to its next right node, just like in
+ * Figure B. The serialized output is in level order as connected by the next
+ * pointers, with '#' signifying the end of each level.
  *
  *
  *
  * Constraints:
  *
  *
- * The number of nodes in the given tree is less than 4096.
- * -1000 <= node.val <= 1000
+ * The number of nodes in the given tree is less than 6000.
+ * -100 <= node.val <= 100
  *
  *
  */
