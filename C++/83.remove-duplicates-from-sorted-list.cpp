45ebfe726d8b5603a12278f3c3ba7d26aec65fde
shanerbo <shanerbo@gmail.com>
2020-09-26 19:43:10 -0700

    q92

---
+++ C++/83.remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=83 lang=cpp
+ *
+ * [83] Remove Duplicates from Sorted List
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (45.50%)
+ * Likes:    1818
+ * Dislikes: 121
+ * Total Accepted:    498.9K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 1->1->2
+ * Output: 1->2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
+ *
+ *
+ */
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *deleteDuplicates(ListNode *head) {
+    if (!head) {
+      return nullptr;
+    }
+    ListNode **prev = &head;
+    ListNode *newHead = head;
+    while (newHead) {
+      if ((*prev)->val != newHead->val) {
+        (*prev)->next = newHead;
+        prev = &(*prev)->next;
+      }
+      newHead = newHead->next;
+    }
+    (*prev)->next = nullptr;
+    return head;
+  }
+};
+// @lc code=end
