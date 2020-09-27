041eded63037b5d28c1a2460cfcfc320caef558b
shanerbo <shanerbo@gmail.com>
2020-09-26 19:15:12 -0700

    q10[D1

---
+++ C++/110.balanced-binary-tree.cpp
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode id=110 lang=cpp
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (43.59%)
+ * Likes:    2594
+ * Dislikes: 181
+ * Total Accepted:    478.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ * For this problem, a height-balanced binary tree is defined as:
+ *
+ *
+ * a binary tree in which the left and right subtrees of every node differ in
+ * height by no more than 1.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Given the following tree [3,9,20,null,null,15,7]:
+ *
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * Return true.
+ *
+ * Example 2:
+ *
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ *
+ *
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ *
+ *
+ * Return false.
+ *
+ */
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int helper(TreeNode *root, bool &res) {
+    if (!root || res == false) {
+      return 0;
+    }
+    int left = 1 + helper(root->left, res);
+    int right = 1 + helper(root->right, res);
+    if (abs(left - right) > 1) {
+      res = false;
+    }
+
+    return max(left, right);
+  }
+  bool isBalanced(TreeNode *root) {
+    bool res = true;
+    helper(root, res);
+    return res;
+  }
+};
+// @lc code=end
