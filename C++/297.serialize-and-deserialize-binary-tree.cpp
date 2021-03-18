3f23994e91564809b33c395bd3d4cf14dfec3522
shanerbo <shanerbo@gmail.com>
2021-03-17 23:07:14 -0700

    q297

--- C++/297.serialize-and-deserialize-binary-tree.cpp
+++ C++/297.serialize-and-deserialize-binary-tree.cpp
@@ -17,33 +17,33 @@
  * sequence of bits so that it can be stored in a file or memory buffer, or
  * transmitted across a network connection link to be reconstructed later in
  * the same or another computer environment.
- * 
+ *
  * Design an algorithm to serialize and deserialize a binary tree. There is no
  * restriction on how your serialization/deserialization algorithm should work.
  * You just need to ensure that a binary tree can be serialized to a string and
  * this string can be deserialized to the original tree structure.
- * 
+ *
  * Example: 
- * 
- * 
+ *
+ *
  * You may serialize the following tree:
- * 
+ *
  * ⁠   1
  * ⁠  / \
  * ⁠ 2   3
  * ⁠    / \
  * ⁠   4   5
- * 
+ *
  * as "[1,2,3,null,null,4,5]"
- * 
- * 
+ *
+ *
  * Clarification: The above format is the same as how LeetCode serializes a
  * binary tree. You do not necessarily need to follow this format, so please be
  * creative and come up with different approaches yourself.
- * 
+ *
  * Note: Do not use class member/global/static variables to store states. Your
  * serialize and deserialize algorithms should be stateless.
- * 
+ *
  */
 #include <algorithm>
 #include <bitset>
@@ -59,15 +59,15 @@
 #include <unordered_set>
 #include <vector>
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
 };
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 using namespace std;
 // @lc code=start
@@ -82,20 +82,37 @@ using namespace std;
  */
 class Codec {
 public:
-
-    // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-        
+  // Encodes a tree to a single string.
+  string serialize(TreeNode *root) {
+    if (!root) {
+      return "x";
     }
+    std::string res = to_string(root->val);
+    auto l = serialize(root->left);
+    auto r = serialize(root->right);
+    res += ' ' + l + ' ' + r;
+    return res;
+  }
 
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        
+  // Decodes your encoded data to tree.
+  TreeNode *deserialize(string data) {
+    stringstream ss(data);
+    return helper(ss);
+  }
+  TreeNode *helper(stringstream &ss) {
+    std::string word;
+    ss >> word;
+    if (word == "x") {
+      return nullptr;
     }
+    TreeNode *root = new TreeNode(stoi(word));
+    root->left = helper(ss);
+    root->right = helper(ss);
+    return root;
+  }
 };
 
 // Your Codec object will be instantiated and called as such:
 // Codec codec;
 // codec.deserialize(codec.serialize(root));
 // @lc code=end
-
