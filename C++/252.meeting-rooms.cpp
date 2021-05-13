9212ff17ea99ed91c36bd91159da8393cda7c34c
shanerbo <shanerbo@gmail.com>
2021-05-12 23:00:40 -0700

    q252 253

---
+++ C++/252.meeting-rooms.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=252 lang=cpp
+ *
+ * [252] meeting rooms
+ */
+
+// @lc code=start
+#include <algorithm>
+#include <vector>
+using namespace std;
+class Solution {
+  public:
+    bool canAttendMeetings(vector<vector<int>> &in) {
+        if (in.empty()) {
+            return true;
+        }
+
+        sort(in.begin(), in.end());
+        for (size_t i = 0; i < in.size() - 1; ++i) {
+            if (in[i][1] > in[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
